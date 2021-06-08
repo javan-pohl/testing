a574283f9503abdb6d9e89fedd8abef9bd5fb2e8
LordBigglesworth <javanpohl@gmail.com>
2021-06-08 14:31:32 -0600

    included error handling for if the params are not a number

--- udacity/add.js
+++ udacity/add.js
@@ -1,5 +1,8 @@
 function add(x, y) {
   let result;
+  if ((typeof x && typeof y) !== 'number') {
+    throw new Error ('Params must be a number')
+  }
   result = x + y;
   console.log(`${x} plus ${y} equals: `, result)
   return result
