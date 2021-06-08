b4449dc9cc9a7355c81848432cf2c3a1113bcaee
LordBigglesworth <javanpohl@gmail.com>
2021-06-08 14:34:10 -0600

    removed the call of the string numbers and am now testing the test of floating point integers

--- udacity/add.js
+++ udacity/add.js
@@ -4,10 +4,13 @@ function add(x, y) {
     throw new Error ('Params must be a number')
   }
   result = x + y;
+  if (parseInt(result) !== result) {
+    console.log(parseInt(result), result);
+  }
   console.log(`${x} plus ${y} equals: `, result)
   return result
 }
 
 add(5,6)
-add('five', 'six')
+// add('five', 'six')
 add(.1, .2)
