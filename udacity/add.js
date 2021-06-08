d8dd6941e9a1a9c68233b49755e1702e240c73c9
LordBigglesworth <javanpohl@gmail.com>
2021-06-08 15:19:53 -0600

    manually testing irregularities realted to floating point problems. the current solution hacks the result down to the tenth place, but I'm searching for a better method

--- udacity/add.js
+++ udacity/add.js
@@ -1,16 +1,24 @@
 function add(x, y) {
   let result;
-  if ((typeof x && typeof y) !== 'number') {
-    throw new Error ('Params must be a number')
+  if ((typeof x && typeof y) !== "number") {
+    throw new Error("Params must be a number");
   }
   result = x + y;
+  // console.log('result.toFixed: ', result.toFixed())
+  // console.log('result.toFixed: ', result.toFixed(1))
+  console.log(x, y);
+  console.log("result: ", result);
   if (parseInt(result) !== result) {
-    console.log(parseInt(result), result);
+    console.log("parseInt: ", parseInt(result), result);
+    console.log(`${x} plus ${y} equals: `, x + y);
+    result = parseFloat(result.toFixed(1));
   }
-  console.log(`${x} plus ${y} equals: `, result)
-  return result
+  console.log(`${x} plus ${y} equals: `, result);
+  return result;
 }
 
-add(5,6)
+// add(5,6)
 // add('five', 'six')
-add(.1, .2)
+add(0.1, 0.2);
+add(0.9, 0.9);
+add(0.234, 0.167);
