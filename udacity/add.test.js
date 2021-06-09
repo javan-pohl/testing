d30e85f3050eea4b33a2f5eff73a5ca1fa94803b
LordBigglesworth <javanpohl@gmail.com>
2021-06-09 11:12:18 -0600

    wrote tests including the one to properly test for the function to throw an error

--- udacity/add.test.js
+++ udacity/add.test.js
@@ -1,5 +1,14 @@
-const sum = require('./add');
+const { expect } = require("@jest/globals");
+const sum = require("./add");
 
-test('add 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3)
-})
+test("add 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test("add(0.234, 0.167) to equal .401", () => {
+  expect(sum(0.234, 0.167)).toBe(0.401);
+});
+test("expect string params to throw error", () => {
+  expect(() => {
+    sum(2, "three")
+  }).toThrow("Params must be a number")
+});
