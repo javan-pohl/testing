72c664744c2ba8d9ce74a6ef0a241fcbcde9546b
LordBigglesworth <javanpohl@gmail.com>
2021-06-10 16:11:30 -0600

    checking to see if I can add ud549 to the repo now

---
+++ ud549/spec/AddressBookSpec.js
@@ -0,0 +1,18 @@
+describe("Address Book", function () {
+  let addressBook, thisContact;
+
+  beforeEach(function () {
+    addressBook = new AddressBook();
+    thisContact = new Contact("bob", "lastBob", "999");
+  });
+  it("should be able to add a contact", function () {
+    addressBook.addContact(thisContact);
+    expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+  it("should be able to delete a contact", function () {
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
+});
