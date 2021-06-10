147ab5189d2e686b1f7bcbe98c82d3b12cdf89f0
LordBigglesworth <javanpohl@gmail.com>
2021-06-10 16:37:13 -0600

    added asynchronous testing to our suite

--- ud549/src/AddressBook.js
+++ ud549/src/AddressBook.js
@@ -1,7 +1,17 @@
 function AddressBook() {
   this.contacts = [];
+  this.initialComplete = false;
 }
+AddressBook.prototype.getInitialContacts = function (cb) {
+  let self = this;
 
+  setTimeout(function () {
+    self.initialComplete = true;
+    if (cb) {
+      return cb();
+    }
+  }, 3);
+};
 AddressBook.prototype.addContact = function (contact) {
   this.contacts.push(contact);
 };
